4a1704b925bacad220e41f02b9a7ff64cbc2b097
Jose Ayerdis <joseayerdis@gmail.com>
2018-10-22 00:06:26 -0400

    La prensa remover code to remove the paywall from newspaper in Nicaragua

---
+++ script.js
@@ -0,0 +1,16 @@
+function waitForElementToDisplay(selector, time) {
+      var element = document.querySelector(selector);
+        if(element) {
+          element.parentElement.removeChild(element);
+          element = document.querySelector('div.tp-backdrop.tp-active');
+          element.parentElement.removeChild(element);
+          return;
+        }
+        else {
+            console.log('Removedor de laprensa!')
+            setTimeout(function() {
+                waitForElementToDisplay(selector, time);
+            }, time);
+        }
+    }
+waitForElementToDisplay('div.tp-modal',2000);
